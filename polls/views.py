5e5024fab83f61585d267aba88a29263cad0615a
Durgnan <durgnan45@gmail.com>
2019-09-08 18:37:55 +0530

    2nd commit

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse as hr
 # Create your views here.
+def index(request):
+    return hr("Hello, World. You're at the Polls index")
+
